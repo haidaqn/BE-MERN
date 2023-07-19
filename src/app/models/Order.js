178e26f91b17dcf9845e304559faa0ecc8ae9ce8
haidang <haidang02032003@gmail.com>
2023-07-19 20:26:31 +0700

    push

--- src/app/models/Order.js
+++ src/app/models/Order.js
@@ -1,28 +1,33 @@
 const mongoose = require('mongoose'); // Erase if already required
 
 // Declare the Schema of the Mongo model
-var orderSchema = new mongoose.Schema({
-    products: [
-        {
-            product: { type: mongoose.Types.ObjectId, ref: 'Product' },
-            count: Number,
-            color: String
+var orderSchema = new mongoose.Schema(
+    {
+        products: [
+            {
+                product: { type: mongoose.Types.ObjectId, ref: 'Product' },
+                count: Number,
+                color: String
+            }
+        ],
+        status: {
+            type: String,
+            default: 'Processing',
+            enum: ['Cancelled', 'Processing', 'Success']
+        },
+        total: Number,
+        coupon: {
+            type: mongoose.Types.ObjectId,
+            ref: 'Coupon'
+        },
+        orderBy: {
+            User: { type: mongoose.Types.ObjectId, ref: 'User' }
         }
-    ],
-    status: {
-        type: String,
-        default: 'Processing',
-        enum: ['Cancelled', 'Processing', 'Success']
     },
-    total: Number,
-    coupon: {
-        type: mongoose.Types.ObjectId,
-        ref: 'Coupon'
-    },
-    orderBy: {
-        User: { type: mongoose.Types.ObjectId, ref: 'User' }
+    {
+        timestamps: true
     }
-});
+);
 
 //Export the model
 module.exports = mongoose.model('Order', orderSchema);
